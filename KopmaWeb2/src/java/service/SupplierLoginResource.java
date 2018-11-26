64cf296e808f8ee31fc5cb5d864aa91652040788
xanadila <BLack-VyperX@Macbook-Matamu>
2018-11-26 22:04:14 +0700

    v2.2

--- KopmaWeb2/src/java/service/SupplierLoginResource.java
+++ KopmaWeb2/src/java/service/SupplierLoginResource.java
@@ -10,6 +10,7 @@ import helper.SupplierHelper;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
@@ -20,6 +21,7 @@ import javax.ws.rs.core.UriInfo;
  *
  * @author MR98X
  */
+@Path("Supplier/login")
 public class SupplierLoginResource {
 
     @Context
